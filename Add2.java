de63b82cdb83e547d7a6f2949ec8001d7fa6a68a
Senthil377 <senthilmsk343@gmail.com>
2023-04-06 11:16:31 +0530

    Ok

---
+++ Add2.java
@@ -0,0 +1,12 @@
+package com.student;
+public class Add2 {
+	public static void main(String[] args) {
+		int temp=0;
+		int temp1=0;
+		for (int i =1; i <=5; i++) {
+			temp=temp+i;
+			temp1=temp1+temp;
+		}System.out.println(temp);
+	}
+
+}
